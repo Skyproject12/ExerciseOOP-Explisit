2fb2d7c869f3f5e20f6087c2f1f448ca6463d65e
sumsky108 <sumendrakadek108@gmail.com>
2019-08-22 10:24:12 +0700

    first commit

---
+++ src/explesit/Explesit.java
@@ -0,0 +1,11 @@
+package explesit;
+public class Explesit {
+
+    public static void main(String[] args) {
+        int i= 2000;
+        byte a=(byte)i;
+        System.out.println("sebelum di casting"+i);
+        System.out.println("sesudah di casting"+a);
+    }
+    
+}
